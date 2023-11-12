b2f57bb208633b8af50f448e61a59620c8459571
hanse <bluewood.truth@gmail.com>
2023-11-12 21:03:17 +0900

    rename components

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import Counter from './lib/Counter.svelte';
-  import UpdateObjTest from './lib/UpdateObjTest.svelte';
+  import Counter from './lib/counter.svelte';
+  import UpdateObjTest from './lib/update-obj-test.svelte';
 </script>
 
 <main>
@@ -13,21 +13,3 @@
     <UpdateObjTest />
   </div>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
